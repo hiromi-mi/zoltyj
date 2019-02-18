e52a9498026314b20905ab0f124c7ecfd8a6ed34
hiromi-mi <hiromi-mi@cat.zaq.jp>
2019-02-18 21:07:20 +0900

    Add license declartion

--- main.rb
+++ main.rb
@@ -1,6 +1,20 @@
 # zoltyj
 # License: Apache-2.0 (See LICENSE)
-# main.rb
+#
+#   Copyright hiromi-mi 2019
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 # https://nokogiri.org/tutorials/parsing_an_html_xml_document.html
 # https://www.rubydoc.info/gems/mastodon-api/Mastodon/Status
 # https://www.rubydoc.info/gems/mastodon-api/Mastodon/REST/Timelines#home_timeline-instance_method
